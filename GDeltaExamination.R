573c64e026c4773e026775e5d58c819daa6fe452
Daniel Moore <dfmoore@gmail.com>
2016-04-04 10:20:36 -0700

    some formating and parameterization

--- GDeltaExamination.R
+++ GDeltaExamination.R
@@ -1,31 +1,52 @@
 # Environment Setup -------------------------------------------------------
-## Environment setup
 # Load packages.
-packages <- c("gdata", "ggplot2", "plyr", "reshape2", "tm", "dplyr", "Matrix", "GDELTtools", "data.table", "date", "lubridate")
+packages <- c("gdata", "ggplot2", "plyr", "reshape2", "tm", "dplyr", "Matrix", "GDELTtools", "data.table",
+              "date", "lubridate")
 packages <- lapply(packages, FUN = function(x) {
     if (!require(x, character.only = TRUE)) {
         install.packages(x)
         library(x, character.only = TRUE)
-    }f
+    }
 })
 
+# Set working directory
+workingdir<-paste("C:\\Users", Sys.getenv("USERNAME"), "Documents\\Github\\EventAnalysis", sep = "\\")
+setwd(workingdir)
+
+DaysToGoBack = 14
+
+#Define Dates
+endstr = format(Sys.Date()-1, format="%Y-%m-%d")
+startstr = format(Sys.Date()-DaysToGoBack-1, format="%Y-%m-%d")
 
 # Load Data ---------------------------------------------------------------
-endstr = format(Sys.Date()-1, format="%m/%d/%y")
-startstr = format(Sys.Date()-8, format="%m/%d/%y")
-
-GDeltLastweek<-data.table(GetGDELT(start.date = startstr, 
+#Actually load the data - ~10-20MB per day
+GDeltData<-data.table(GetGDELT(start.date = startstr, 
          end.date = endstr, 
          data.url.root = "http://data.gdeltproject.org/events/",
          verbose = TRUE))
-
-GDeltLastweek$Date1<-parse_date_time(as.character(GDeltLastweek$SQLDATE), order="ymd")
 
 #Load event codes
 eventcodes<-read.table("http://gdeltproject.org/data/lookups/CAMEO.eventcodes.txt", header=TRUE, sep="\t", as.is=TRUE, colClasses=c("character","character"))
 
     
 # Format Tables------------------------------------------------------------
-EventsByDay<-GDeltLastweek[,(COUNT = .N),by=c("Date1", "EventRootCode")]
-EventsByCode<-GDeltLastweek[,(COUNT = .N), by=c("EventRootCode")]
-ggplot(EventsByDay, aes(Date1, V1))+geom_bar(stat="identity")
+GDeltData$Date1<-parse_date_time(as.character(GDeltData$SQLDATE), order="ymd")
+GDeltData<-merge(GDeltData, eventcodes, by.x = "EventRootCode", by.y = "CAMEOEVENTCODE")
+
+USOnlyGDelt<-GDeltData[Actor2CountryCode=="USA",] # | Actor2CountryCode=="USA",]
+
+EventsByDay<-GDeltData[,(COUNT = .N),by=c("Date1", "EventRootCode", "EVENTDESCRIPTION")]
+EventsByCode<-GDeltData[,(COUNT = .N), by=c("EventRootCode", "EVENTDESCRIPTION")]
+EventsByCode<-EventsByCode[order(-V1)]
+
+USOnlyEventsByDay<-USOnlyGDelt[,(COUNT = .N),by=c("Date1", "EventRootCode", "EVENTDESCRIPTION")]
+USOnlyEventsByCode<-USOnlyGDelt[,(COUNT = .N), by=c("EventRootCode", "EVENTDESCRIPTION")]
+USOnlyEventsByCode<-USOnlyEventsByCode[order(-V1)]
+
+
+ggplot(EventsByCode, aes(EVENTDESCRIPTION, V1))+geom_bar(stat="identity")
+ggplot(EventsByDay, aes(x=Date1, y=V1))+geom_line(aes(colour=EVENTDESCRIPTION))
+ggplot(USOnlyEventsByCode, aes(EVENTDESCRIPTION, V1))+geom_bar(stat="identity")
+ggplot(USOnlyEventsByDay, aes(x=Date1, y=V1))+geom_line(aes(colour=EVENTDESCRIPTION))
+
